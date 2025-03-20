ee0f460b9827ca7451448a02925c83d68e988418
McCancer <mkunzer@DESKTOP-VOGQ053>
2025-03-19 21:45:06 -0500

    Adding line drawing

--- src/Window.py
+++ src/Window.py
@@ -23,9 +23,29 @@ class Window:
     def close(self):
         self.running = False
 
+    def draw_line(self, Line, fill_color):
+        Line.draw(self.canvas,fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self, pt1, pt2):
+        self.pt1 = pt1
+        self.pt2 = pt2
+
+    def draw(self, Canvas, fill_color):
+        Canvas.create_line(self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y, fill=fill_color, width=2)
+
 
 def main():
     win = Window(800, 600)
+    pt1 = Point(100, 200)
+    pt2 = Point(200, 500)
+    ln = Line(pt1, pt2)
+    win.draw_line(ln, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
