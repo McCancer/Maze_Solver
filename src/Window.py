3a06d796f1d9e1787f805b17b13cbdab09c025cd
McCancer <mkunzer@DESKTOP-VOGQ053>
2025-03-21 19:20:13 -0500

    Create Cells L5

--- src/Window.py
+++ src/Window.py
@@ -39,13 +39,34 @@ class Line:
     def draw(self, Canvas, fill_color):
         Canvas.create_line(self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y, fill=fill_color, width=2)
 
+class Cell:
+    def __init__(self, pt1, pt2, can):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.top_left_point = pt1
+        self.bottom_right_point = pt2
+        self.__win = can
+
+    def draw(self):
+        pt_bottom_left = Point(self.top_left_point.x, self.bottom_right_point.y)
+        pt_top_right = Point(self.bottom_right_point.x, self.top_left_point.y)
+        if(self.has_left_wall):
+            self.__win.draw_line(Line(self.top_left_point, pt_bottom_left), 'black')
+        if(self.has_right_wall):
+            self.__win.draw_line(Line(self.bottom_right_point, pt_top_right), 'black')
+        if(self.has_top_wall):
+            self.__win.draw_line(Line(self.top_left_point, pt_top_right), 'black')
+        if(self.has_bottom_wall):
+            self.__win.draw_line(Line(self.bottom_right_point, pt_bottom_left), 'black')
 
 def main():
     win = Window(800, 600)
     pt1 = Point(100, 200)
     pt2 = Point(200, 500)
-    ln = Line(pt1, pt2)
-    win.draw_line(ln, "black")
+    cell1 = Cell(pt1, pt2, win)
+    cell1.draw()
     win.wait_for_close()
 
 if __name__ == "__main__":
