a6f958301da1bb4a2e558bd88062cccc776a0c7b
McCancer <mkunzer@DESKTOP-VOGQ053>
2025-03-29 14:36:09 -0500

    L12 Maze now gets solved

--- src/Maze.py
+++ src/Maze.py
@@ -108,6 +108,41 @@ class Maze:
             for j in range(len(self._cells[i])):
                 self._cells[i][j].visited = False
             
+    def solve(self):
+        return self._solve_r(0, 0)
+
+    def _solve_r(self, i, j):
+        self.__animate()
+        self._cells[i][j].visited = True
+        if(i == self.num_cols-1 and j == self.num_rows-1):
+            return True
+        if(i+1 < self.num_cols and (not self._cells[i+1][j].visited) and (not self._cells[i][j].has_right_wall)):
+            #right
+            self._cells[i][j].draw_move(self._cells[i+1][j])
+            if(self._solve_r(i+1,j)):
+                return True
+            self._cells[i][j].draw_move(self._cells[i+1][j], True)
+        if(i-1 >= 0 and (not self._cells[i-1][j].visited) and (not self._cells[i][j].has_left_wall)):
+            #left
+            self._cells[i][j].draw_move(self._cells[i-1][j])
+            if(self._solve_r(i-1,j)):
+                return True
+            self._cells[i][j].draw_move(self._cells[i-1][j], True)
+        if(j+1 < self.num_rows and (not self._cells[i][j+1].visited) and (not self._cells[i][j].has_bottom_wall)):
+            #bottom
+            self._cells[i][j].draw_move(self._cells[i][j+1])
+            if(self._solve_r(i,j+1)):
+                return True
+            self._cells[i][j].draw_move(self._cells[i][j+1], True)
+        if(j-1 >= 0 and (not self._cells[i][j-1].visited) and (not self._cells[i][j].has_top_wall)):
+            #top
+            self._cells[i][j].draw_move(self._cells[i][j-1])
+            if(self._solve_r(i,j-1)):
+                return True
+            self._cells[i][j].draw_move(self._cells[i][j-1], True)    
+        return False
+        
+
 def main():
     pass
 
